01ba8e50591242892732305d2e8e6398f4c2058f
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2026-03-23 18:29:12 +0300

    odd_even

---
+++ odd_even.py
@@ -0,0 +1,8 @@
+nums = int(input("Enter any number : "))
+if nums<=0:
+    print("Negative number not allowed")
+    
+elif nums%2 == 0:
+        print("The number is even")
+else : 
+        print("Then number id odd")
